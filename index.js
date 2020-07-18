51954fd04f9e85f832c99f04f1c7d61edb6633ef
Pawanachai <Kanpim5@gmail.com>
2020-07-18 14:18:56 +0700

    DOM Manipulation

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-/* Flow controls*/
+/* DOM Manipulation*/
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
+/* 
+
+document.querySelector() = เอา tag มาใช้
+document.getElementById() = เอา ID มาใช้
+document.createElement() = สร้าง element
+document.appendChild() = สร้ง element ต่อจาก element ก่อนหน้า (Root => Child)
+
+*/
+
+function appendImageElem(keyword, index) {
+  const imgElem = document.createElement("img");
+  imgElem.src = `https://source.unsplash.com/400x225/?${keyword}&sig${index}`;
+
+  const galleryElem = document.querySelector(".gallery");
+  galleryElem.appendChild(imgElem);
 }
+
+function run() {
+  for (let i = 0; i <= 9; i++) {
+      appendImageElem("cat", i);
+      
+  }
+}
+
+run();
